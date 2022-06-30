49777df588222ffdff01439fbad467f8e3aab0e1
ananya23101 <ananya23102001@gmail.com>
2022-06-30 16:30:41 +0530

    changes

--- js.js
+++ js.js
@@ -1,3 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    
+    hamburger.classList.toggle("toggle");
+});
 var button = document.getElementById('q1');
 var answer = document.getElementById('qa-answer');
 button.addEventListener('click', runEvent);
